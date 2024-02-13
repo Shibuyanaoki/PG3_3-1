bf06dd5bd48aaf491d059074fed9096de9c61cd4
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2024-02-13 16:51:51 +0900

    完成

--- Player.h
+++ Player.h
@@ -1,15 +1,23 @@
-#pragma once
+﻿#pragma once
+#include "Vector2.h"
 class Player
 {
 public:
+
+	Player();
 
 	void Init();
 	void Update();
 	void Draw();
 
+	// キーごとのコマンド
 	void MoveRight();
 	void MoveLeft();
 
+private:
+
+	Vector2 pos_;
+	float speed_;
 
 };
 
